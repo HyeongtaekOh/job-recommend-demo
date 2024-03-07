d12bfd7883e930128012a82b1d53c4abedb885a2
오형택 <oh052679@gmail.com>
2024-03-08 01:30:08 +0900

    Feat: apply double weight to requiredTechStack

--- src/main/scala/Similarity.scala
+++ src/main/scala/Similarity.scala
@@ -1,6 +1,6 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.ml.feature.CountVectorizer
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.ml.linalg.{SparseVector, Vector, Vectors}
 import org.apache.spark.sql.functions.udf
 
@@ -19,24 +19,27 @@ object Similarity {
     import spark.implicits._
 
     // 예시 데이터 (실제 데이터 로딩 로직 필요)
+    val userProfiles = Seq(
+      (1, "Backend Developer", Seq("Java", "Spring", "Docker", "Kubernetes", "AWS", "MySQL", "Git")),
+      (2, "Data Scientist", Seq("Python", "R", "TensorFlow", "Keras", "Pandas", "NumPy", "Scikit-learn"))
+    ).toDF("userId", "position", "techStack")
+
+    val jobPostings0 = Seq(
+      (1, "Backend Developer", Seq("Java", "Spring Boot", "MongoDB", "Docker", "AWS", "Git", "Jenkins"), Seq("Kubernetes", "Ansible", "Terraform")),
+      (2, "Data Scientist", Seq("Python", "R", "SQL", "TensorFlow", "PyTorch"), Seq("Apache Spark", "Hadoop", "Keras"))
+    ).toDF("jobId", "position", "requiredTechStack", "preferredTechStack")
 //    val userProfiles = Seq(
-//      (1, "Backend Developer", Seq("Java", "Spring", "Docker", "Kubernetes", "AWS", "MySQL", "Git")),
-//      (2, "Data Scientist", Seq("Python", "R", "TensorFlow", "Keras", "Pandas", "NumPy", "Scikit-learn"))
-//    ).toDF("userId", "position", "techStack")
+//      (0, Array("Java", "Python")),
+//      (1, Array("Python", "Scala"))
+//    ).toDF("userId", "techStack")
 //
 //    val jobPostings = Seq(
-//      (1, "Backend Developer", Seq("Java", "Spring Boot", "MongoDB", "Docker", "AWS", "Git", "Jenkins"), Seq("Kubernetes", "Ansible", "Terraform")),
-//      (2, "Data Scientist", Seq("Python", "R", "SQL", "TensorFlow", "PyTorch"), Seq("Apache Spark", "Hadoop", "Keras"))
-//    ).toDF("jobId", "position", "requiredTechStack", "preferredTechStack")
-    val userProfiles = Seq(
-      (0, Seq("Java, Python")),
-      (1, Seq("Python, Scala"))
-    ).toDF("userId", "techStack")
+//      (0, Array("Python", "Scala")),
+//      (1, Array("Java", "Scala"))
+//    ).toDF("jobId", "techStack")
 
-    val jobPostings = Seq(
-      (0, Seq("Python, Scala")),
-      (1, Seq("Java, Scala"))
-    ).toDF("jobId", "techStack")
+    val jobPostings = jobPostings0.map(row => (row.getInt(0), row.getString(1), row.getSeq[String](2) ++ row.getSeq[String](2) ++ row.getSeq[String](3)))
+      .toDF("jobId", "position", "techStack")
 
     // CountVectorizer를 사용하여 기술 스택 벡터화
     val cvModel = new CountVectorizer()
@@ -44,11 +47,12 @@ object Similarity {
       .setOutputCol("features")
       .fit(userProfiles.select("techStack").union(jobPostings.select("techStack")))
 
-    userProfiles.select("techStack").union(jobPostings.select("techStack")).show()
+    println(cvModel.vocabulary.mkString("Array(", ", ", ")"))
     println("cvModel.vocabulary = ", cvModel.vocabulary.mkString("Array(", ", ", ")"))
+    cvModel.vocabulary.foreach(println)
 
-    val userFeatures = cvModel.transform(userProfiles)
-    val jobFeatures = cvModel.transform(jobPostings)
+    val userFeatures = cvModel.transform(userProfiles).toDF("userId", "position", "techStack", "features_user")
+    val jobFeatures = cvModel.transform(jobPostings).toDF("jobId", "position", "techStack", "features_job")
 
     userFeatures.show()
     jobFeatures.show()
@@ -62,6 +66,7 @@ object Similarity {
       val valuesA = vectorA.values
       val indicesB = vectorB.indices
       val valuesB = vectorB.values
+
 
       var dotProduct = 0.0
       var normA = 0.0
@@ -93,13 +98,15 @@ object Similarity {
 
     // 유사도 계산
     val similarityScores = userFeatures.crossJoin(jobFeatures).map { row =>
-      val userVec = row.getAs[SparseVector](2)
-      val jobVec = row.getAs[SparseVector](5)
+      val userVec = row.getAs[SparseVector]("features_user")
+      val jobVec = row.getAs[SparseVector]("features_job")
       println("userVec = ", userVec)
       println("jobVec = ", jobVec)
+      println(row)
       val similarity = cosineSimilarity(userVec, jobVec)
-      (row.getInt(0), row.getInt(3), similarity) // (userId, jobId, similarityScore)
+      (row.getAs[Int]("userId"), row.getAs[Int]("jobId"), similarity) // (userId, jobId, similarityScore)
     }.toDF("userId", "jobId", "similarityScore")
+      .sort($"userId", $"similarityScore".desc)
 
     similarityScores.show()
   }
